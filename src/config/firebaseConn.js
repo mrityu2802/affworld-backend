a730485c2179aa773c00c9344ced3ee2b12bfb3d
mrityu2802 <mmrityunjay727@gmail.com>
2025-01-26 17:31:58 +0530

    env fix

--- src/config/firebaseConn.js
+++ src/config/firebaseConn.js
@@ -1,6 +1,20 @@
-import serviceAccount from "./firebaseSA.json" assert { type: "json" };
+// import serviceAccount from "./firebaseSA.json" assert { type: "json" };
 import admin from "firebase-admin";
 
+const key = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key.replace(/\\n/g, "\n"),
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain,
+};
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(key),
 });
